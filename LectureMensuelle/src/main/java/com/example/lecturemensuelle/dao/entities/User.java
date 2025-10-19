b77dc1325f0520beca4966d3e2a559394c2f39cf
Mitsumandot <chiguer.othmane@ensam-casa.ma>
2025-10-19 22:00:59 +0100

    jwtService class

--- LectureMensuelle/src/main/java/com/example/lecturemensuelle/dao/entities/User.java
+++ LectureMensuelle/src/main/java/com/example/lecturemensuelle/dao/entities/User.java
@@ -1,11 +1,13 @@
 package com.example.lecturemensuelle.dao.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 
 @Getter
@@ -13,12 +15,12 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true,  nullable = false)
+    @Column(unique = true, nullable = false)
     private String username;
     @Column(nullable = false)
     private String password;
@@ -26,5 +28,34 @@ public class User {
     private String email;
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private List<BookUser> books;
+
+
+    @Getter(AccessLevel.NONE)
+    private boolean enabled;
+
+
+    @Column(name = "verification_code")
+    private String verificationCode;
+    @Column(name = "verification_expiration")
+    private LocalDateTime verificationCodeExpireAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return List.of();
+    }
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+
+    public boolean isEnabled(){
+        return enabled;
+
+    }
+
 
 }
--- LectureMensuelle/src/main/java/com/example/lecturemensuelle/dao/repositories/UserRepository.java
+++ LectureMensuelle/src/main/java/com/example/lecturemensuelle/dao/repositories/UserRepository.java
@@ -2,8 +2,15 @@ package com.example.lecturemensuelle.dao.repositories;
 
 import com.example.lecturemensuelle.dao.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByVerificationCode(String verificationCode);
+
 
 
 }
